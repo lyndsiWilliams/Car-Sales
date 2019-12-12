d78c51bc47dd529448736774e13e15c6f21225a6
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-12 07:09:18 -0600

    Cleaned up the code a little

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+// Components
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-import { dispatch } from 'C:/Users/kcatg/AppData/Local/Microsoft/TypeScript/3.6/node_modules/rxjs/internal/observable/pairs';
 
 const App = props => {
-  // const state = {
-  //   additionalPrice: 0,
-  //   car: {
-  //     price: 26395,
-  //     name: '2019 Ford Mustang',
-  //     image:
-  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-  //     features: []
-  //   },
-  //   additionalFeatures: [
-  //     { id: 1, name: 'V-6 engine', price: 1500 },
-  //     { id: 2, name: 'Racing detail package', price: 1500 },
-  //     { id: 3, name: 'Premium sound system', price: 500 },
-  //     { id: 4, name: 'Rear spoiler', price: 250 }
-  //   ]
-  // };
-
-  // const removeFeature = item => {
-  //   // dispatch an action here to remove an item
-  //   dispatch({ type: "REMOVE_ITEM", payload: item.id });
-  // };
-
-  // const buyItem = item => {
-  //   // dipsatch an action here to add an item
-  //   dispatch({ type: "ADD_ITEM", payload: item.id })
-  // };
 
   return (
     <div className="boxes">
@@ -57,6 +31,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(
-  mapStateToProps
-)(App);
+export default connect(mapStateToProps)(App);
